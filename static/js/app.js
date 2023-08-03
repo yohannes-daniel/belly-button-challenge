460cf9056914645d4c93599d618762bc31e544a7
yohannes-daniel <yohannesd83@gmail.com>
2023-08-03 14:12:50 -0400

    Update app file

--- static/js/app.js
+++ static/js/app.js
@@ -2,8 +2,6 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // Make an empty list to store the 
-
-
 let jsonData = d3.json(url).then(function(data) {
     // console.log(data.samples[0]);
 
